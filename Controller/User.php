9d4f56f6450f699bd4f5da93fc5a4b355324da43
DimSan <DiS-ne@ukr.net>
2015-07-14 13:26:31 +0300

    first commit

---
+++ Controller/User.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of User
+ *
+ * @author admin
+ */
+class Controller_User 
+{
+    public function profileAction()
+            {
+            echo 'This';
+            }
+}
